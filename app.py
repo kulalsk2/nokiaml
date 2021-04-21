c15c3116dbdb4f85a044f2b3cb487bce44319ee6
kulalsk2 <subramanyakulal2000@gmail.com>
2021-04-21 14:58:30 +0530

    min max equation updated

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask,render_template,request
 import pickle
 
 model = pickle.load(open('model.pkl','rb'))
-
+max = [1.0,10.0,28.18,25.02,97.85,24.83,29.87,30.67,40245,2017,12,31]
+min = [0.0,1.15,-16.06,-18.72,21.25,1.03,8.59,29.48,580,2016,1,1]
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,9 +22,14 @@ def home():
     windspeed = float(request.form['windspeed'])
     stationpressure = float(request.form['stationpressure'])
     altimeter = float(request.form['altimeter'])
-    arr = list([[cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure,altimeter,0.0,0.727273,0.433333]])
-    print(arr)   
-    pred = model.predict(arr)
+    X = list([cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure,altimeter,2017,7,5])
+    Xmax = [1.0,10.0,28.18,25.02,97.85,24.83,29.87,30.67,2017,12,31]
+    Xmin = [0.0,1.15,-16.06,-18.72,21.25,1.03,8.59,29.48,2016,1,1]
+    XN= [x-xmn for x, xmn in zip(X, Xmin)]
+    XD = [xmx-xmn for xmx, xmn in zip(Xmax, Xmin)]
+    scaled =  [xn / xd for xn, xd in zip(XN, XD)]
+    scaled = list([scaled])
+    pred = model.predict(scaled)
     print(pred)
     return render_template('after.html',data=pred)
 
@@ -36,3 +42,4 @@ if(__name__=="__main__"):
 
 #Xsc=(X−Xmin)/(Xmax−Xmin)
 #0.52	0.849718	0.847425	0.860949	0.798447	0.243697	0.977444	0.596639	0.0	0.727273	0.433333
+#X = [0.03,9.29,25.32,21.64,79.37,4.97,29.28,30.08,2017,7,5]
