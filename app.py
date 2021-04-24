c834c611e47b119f52aa876b3347a958ab21ea09
kulalsk2 <subramanyakulal2000@gmail.com>
2021-04-25 00:39:07 +0530

    Type conversion is done still getting f*** up prediction

--- app.py
+++ app.py
@@ -12,12 +12,9 @@ app = Flask(__name__)
 def man():
     return render_template('index.html') #this by itself selects template folder
 
-@app.route('/predict',methods=['POST'])
-def home():
-    cityname = request.form['cityname']
-    print(cityname)
+def latandlong(cityname):
     urlRapid = "https://community-open-weather-map.p.rapidapi.com/find"
-    querystringRapid = {"q":"london","mode":"null","lon":"0","type":"link, accurate","lat":"0","units":"imperial, metric"}
+    querystringRapid = {"q":cityname,"mode":"null","lon":"0","type":"link, accurate","lat":"0","units":"imperial, metric"}
     headersRapid = {
     'x-rapidapi-key': "38dbf9e34dmshbfaff0223d0ac4fp180cc3jsnb22b375a7fad",
     'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
@@ -26,6 +23,9 @@ def home():
     resFromRapid = res.json()
     lat = float(resFromRapid['list'][0]['coord']['lat'])
     long = float(resFromRapid['list'][0]['coord']['lon'])
+    return lat,long
+
+def getWeatherData(lat,long):
     urlWeatherBit = "https://api.weatherbit.io/v2.0/current?lat="+str(lat)+"&lon="+str(long)+"&key=f6bba80d9ad242b4b82bfb54364059ea"
     res = requests.request("GET", urlWeatherBit)
     resFromWebit = res.json()
@@ -36,11 +36,32 @@ def home():
     relativehumidity = float(resFromWebit['data'][0]['rh'])
     windspeed = float(resFromWebit['data'][0]['wind_spd'])
     stationpressure = float(resFromWebit['data'][0]['pres'])
-    urlAltitude = "https://maps.googleapis.com/maps/api/elevation/json?locations="+str(lat)+","+str(long)+"&key=AIzaSyBlLBkfaeA_plfPMGkMBZs6TNc-uuHNiek"
-    res = requests.request("GET", urlAltitude)
-    resAltimeter = res.json()
-    altimeter=resAltimeter['results']['elevation']
-    X = list([cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure,30.8,2017,7,5])
+    urlAltitude = "https://api.open-elevation.com/api/v1/lookup?locations="+str(lat)+","+str(long)+""
+    resAltimeter = requests.request("GET", urlAltitude).json()
+    altimeter=float(resAltimeter['results'][0]['elevation'])
+    X = [cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure,altimeter,2017,7,5]
+    return X
+    
+def getCorrectUnit(X):
+    X[1] = 1.60934449789 *X[1] #1/0.621371 
+    X[5] = 0.44703925898*X[5]   #1/2.23694 
+    pmb = X[6]
+    hm = X[7]
+    X[6] =  33.8638672536 *X[6] #1/0.02953
+    pmbmin0_3 = pmb-0.3
+    pmbmin0_3toPower0_190284 = pmbmin0_3**0.190284
+    hmBYpmbmin0_3toPower0_190284 = hm/pmbmin0_3toPower0_190284
+    rightEq = (1+0.0000842288*hmBYpmbmin0_3toPower0_190284)**5.25530260032
+    X[7] = pmbmin0_3*rightEq
+    return X
+
+@app.route('/predict',methods=['POST'])
+def home():
+    cityname = request.form['cityname']
+    lat,long = latandlong(cityname)
+    X = getWeatherData(lat,long)
+    X = getCorrectUnit(X)
+    X = list(X)
     Xmax = [1.0,10.0,28.18,25.02,97.85,24.83,29.87,30.67,2017,12,31]
     Xmin = [0.0,1.15,-16.06,-18.72,21.25,1.03,8.59,29.48,2016,1,1]
     XN= [x-xmn for x, xmn in zip(X, Xmin)]
