c8c63394d694a39026dee012faba8563ad6fede5
K Shivanithyanathan <shivanithyanathan.ec18@bmsce.ac.in>
2021-04-28 15:40:04 +0530

    Found Hourly Data and Summed up the solar Energy
    Output nearly at 28K to 30K

--- app.py
+++ app.py
@@ -29,54 +29,66 @@ def getWeatherData(lat,long):
     urlWeatherBit = "https://api.weatherbit.io/v2.0/history/hourly?lat="+str(lat)+"&lon="+str(long)+"&start_date=2021-04-26&end_date=2021-04-27&tz=local&key=f6bba80d9ad242b4b82bfb54364059ea"
     res = requests.request("GET", urlWeatherBit)
     resFromWebit = res.json()
-    visibility = float(resFromWebit['data'][0]['vis'])
-    cloudcoverage = float(resFromWebit['data'][0]['clouds'])
-    temperature = float(resFromWebit['data'][0]['temp'])
-    dewpoint = float(resFromWebit['data'][0]['dewpt'])
-    relativehumidity = float(resFromWebit['data'][0]['rh'])
-    windspeed = float(resFromWebit['data'][0]['wind_spd'])
-    stationpressure = float(resFromWebit['data'][0]['pres'])
+    return resFromWebit
+
+def locElevation(lat, long):
     urlAltitude = "https://api.opentopodata.org/v1/aster30m?locations="+str(lat)+","+str(long)+""  
     resAltimeter = requests.request("GET", urlAltitude).json()
     altimeter=float(resAltimeter['results'][0]['elevation'])
+    return altimeter
+
+def refinedData(resFromWebit, i):
+    visibility = float(resFromWebit['data'][i]['vis'])
+    cloudcoverage = float(resFromWebit['data'][i]['clouds'])
+    temperature = float(resFromWebit['data'][i]['temp'])
+    dewpoint = float(resFromWebit['data'][i]['dewpt'])
+    relativehumidity = float(resFromWebit['data'][i]['rh'])
+    windspeed = float(resFromWebit['data'][i]['wind_spd'])
+    stationpressure = float(resFromWebit['data'][i]['pres'])
+    hour = str(resFromWebit['data'][i]['timestamp_local'])
+    hour = int(hour[11:13])
     # todayData = date.today().strftime("%d-%m-%Y")
     # day,month,year = tuple(str(todayData).split('-'))
     # year = int(year)
     # month = int(month)
     # day = int(day)
-    X  = [cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure,altimeter]
+    X  = [hour,cloudcoverage,visibility,temperature,dewpoint,relativehumidity,windspeed,stationpressure]
     return X
 
 def getCorrectUnit(X):
-    X[0] = X[0]/100
-    X[1] = 0.621371 *X[1] #0.621371 
-    X[5] = 2.23694 *X[5]   #2.23694 
-    pmb = X[6]
-    hm = X[7]
-    X[6] = 0.02953 *X[6] #0.02953
+    X[1] = X[1]/100
+    X[2] = 0.621371 *X[2] #0.621371 
+    X[6] = 2.23694 *X[6]   #2.23694 
+    pmb = X[7]
+    hm = X[8]
+    X[7] = 0.02953 *X[7] #0.02953
     pmbmin0_3 = pmb-0.3
     pmbmin0_3toPower0_190284 = pmbmin0_3**0.190284
     hmBYpmbmin0_3toPower0_190284 = hm/pmbmin0_3toPower0_190284
     rightEq = (1+0.0000842288*hmBYpmbmin0_3toPower0_190284)**5.25530260032
-    X[7] = pmbmin0_3*rightEq
-    X[7]= 0.02953*X[7]
+    X[8] = pmbmin0_3*rightEq
+    X[8]= 0.02953*X[8]
     return X
 
 @app.route('/predict',methods=['POST'])
 def home():
     cityname = request.form['cityname']
     lat,long = latandlong(cityname)
-    X = getWeatherData(lat,long)
-    X = getCorrectUnit(X)
-    print(X)
-    timestamp = time.strftime('%H:%M:%S')
-    hour =  int(timestamp[:2])
-    X.insert(0,hour)
-    print(X)
-    X = list([X])
-    pred = model.predict(X)
-    print(pred)
-    return render_template('after.html',data=pred)
+    resFromWebit = getWeatherData(lat,long)
+    altimeter = locElevation(lat, long)
+    final = list()
+    for i in range(0,24):
+        X = refinedData(resFromWebit, i)
+        X.append(altimeter)
+        X = getCorrectUnit(X)
+        print(X)
+        X = list([X])
+        pred = model.predict(X)
+        final.append(pred)
+        X = []
+    print(final)
+    output = sum(final)
+    return render_template('after.html',data=output)
 
 
     
